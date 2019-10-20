35b7ec8d4c4803f6fd15456b50f4f03850fadebd
Matheus L <30654908+MatheusAL@users.noreply.github.com>
2019-10-20 16:54:19 -0300

    Adicionando centésimo de segundo

--- relogio.v
+++ relogio.v
@@ -23,6 +23,23 @@ always @(posedge clk or negedge reset)
      end
 endmodule
 
+module count100(input clk, input rst, input ud, output [6:0] Count, output loop);  reg [6:0] Count;
+  always @ (posedge clk or negedge rst)
+begin
+  if (~rst) 
+    Count <= 0;   // reset register
+  else if ( Count == 99  & ud) 
+    Count <= 0; 
+  else if (Count == 0 & ~ud)
+    Count <= 99;
+  else if (ud)
+    Count <= Count + 1;  // increment register
+  else
+    Count <= Count - 1;
+end
+assign loop = (Count == 99)&ud |  (Count==0) & ~ud;
+endmodule
+
 module count59(input clk, input rst, input ud, output [5:0] Count, output loop);  reg [5:0] Count;
   always @ (posedge clk or negedge rst)
 begin
@@ -59,10 +76,11 @@ endmodule
 
 
 module relogio(input clk, res, startstop, zera, ud,
-               input rc, output [5:0] segundos,minutos,
+               input rc, output [6:0]centsegundos,output [5:0] segundos,minutos,
                output[4:0] hora);
-wire outs,outs1,routs,routs1;
+wire outs,outs1,outs2,routs,routs1,routs2;
 wire enable, nr;
+wire [6:0] centsc,centsr;	//centesimo de segundo
 wire [5:0] sc,sr,mc,mr;
 wire [4:0] hc,hr;
 
@@ -70,19 +88,22 @@ assign enable = startstop & clk;
 assign nr = res & zera;
 
 /****** Cronometro *******/
-count59 seg(enable,nr,ud,sc,outs);
-count59 min(~outs,nr,ud,mc,outs1);
+count100 centseg(clk,res,ud,centsc,outs);
+count59 seg(enable,nr,ud,sc,outs1);
+count59 min(~outs,nr,ud,mc,outs2);
 count23 hor(~outs1,nr,ud,hc);
 
 /********* Relogio *********/
-count59 rseg(clk,res,1'b1,sr,routs);
-count59 rmin(~routs,res,1'b1,mr,routs1);
-count23 rhor(~routs1,res,1'b1,hr);
+count100 rcentseg(clk,res,1'b1,centsr,routs);
+count59 rseg(~routs,res,1'b1,sr,routs1);
+count59 rmin(~routs1,res,1'b1,mr,routs2);
+count23 rhor(~routs2,res,1'b1,hr);
 
 /********* MUX *********/
 assign hora = (nrc)? hr:hc;
 assign minutos = (nrc)? mr:mc;
 assign segundos = (nrc)? sr:sc;
+assign centsegundos = (nrc)?centsr:centsc; 
 
 wire nrc;
 statem maq(clk,res,rc,nrc);
@@ -90,9 +111,9 @@ statem maq(clk,res,rc,nrc);
 endmodule
 
 module top(input clk, res, startstop, zera, ud,
-               input rc,
+               input rc,output [6:0] centsegundos,
                output [5:0] segundos,minutos,
                output[4:0] hora);
   relogio r(clk, res, startstop, zera, ud,
-               rc,segundos,minutos,hora);
+               rc, centsegundos,segundos,minutos,hora);
 endmodule
